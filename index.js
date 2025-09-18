1416d2700510ee9ff247ca1b9a27522f34d4f874
dalia-hegazy <daliahegazy346@gmail.com>
2025-09-19 02:08:11 +0300

    back

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+// 
 import express from "express";
+import dotenv from "dotenv";
 import bootstrap from "./src/app.controller.js";
+
+dotenv.config(); // عشان يقرأ DB_URL من .env
+
 const app = express();
-bootstrap(app,express);
-const port = 3000;
-app.listen(port, ()=>{
-    console.log("server is running in port",port);
+bootstrap(app, express);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("🚀 server is running in port", port);
 });
